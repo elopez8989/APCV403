f15f9b5511c6210a7442b6331623245af1b27b33
elopez8989 <156391771+elopez8989@users.noreply.github.com>
2024-02-18 19:34:52 -0800

    Add files via upload

---
+++ mod6.js
@@ -0,0 +1,8 @@
+document.querySelector('#amazon')
+.addEventListener('click', ()=> {
+   window.location.href = 'https://www.amazon.com/Marshmallow-Variety-Marshmallows-Assorted-Dessert/dp/B09RLRHFY7/ref=sr_1_8?crid=51SMK8HG5MEI&dib=eyJ2IjoiMSJ9.wSj4THK_e_4T7-7edeSp5ZTpS19_8_Vdxi-mdmUWGa1R2EGmA9IaHq8bYqUyMLT2-yX6udhXtbQsLuDfgEe9ix6nbMsBa5WGqQ9UiiMocRWxgYd-sj4pw5CeyueUvduVhtkqFq-1VDlS16SciNLDANjRxMjHNeA8mqIRCkLrlP1ZfYkIBo7NqTajjQiUaI4-Z0_Abz29yicHD8tm77xtnFAc_LAXEn2XXFH6f8PkHl0b6LWh4kKwGlSwcUFPRareu6DJBgiBXJbSLG3irkdnN5NpGlPAaQGWH09jrVScgig.CaOlROdgql4fHUZ4O3dQxuKCxw2FXmdu8Ls0BmDZqyk&dib_tag=se&keywords=peeps&qid=1708237182&sprefix=peep%2Caps%2C267&sr=8-8';
+});
+
+function showanswer(){
+    window.alert("10");
+}
